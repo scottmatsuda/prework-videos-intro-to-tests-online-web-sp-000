a1cf09e24e30a9a138c1ba60661d1f77544feabd
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-09 19:07:04 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -8,3 +8,12 @@ describe "conversions" do
     end
   end
 end
+
+describe "conversions" do
+  describe '#ounces_to_grams' do
+    it 'given one, returns 28.3495' do
+      grams = ounces_to_grams(0)
+      expect(grams).to eq(0.0)
+    end
+  end
+end
